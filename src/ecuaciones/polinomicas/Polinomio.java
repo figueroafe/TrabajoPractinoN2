ef8443fc69151783f640030d8688df2286884397
Francisco Figueroa <figueroafe@gmail.com>
2017-09-09 13:26:22 -0300

    Constructores

--- src/ecuaciones/polinomicas/Polinomio.java
+++ src/ecuaciones/polinomicas/Polinomio.java
@@ -1,5 +1,12 @@
 package ecuaciones.polinomicas;
 
 public class Polinomio {
+	private int grado;
+	private double[] coeficientes;
+
+	public Polinomio(int grado, double[] coeficientes) {
+		this.grado = grado;
+		this.coeficientes = coeficientes;
+	}
 
 }
